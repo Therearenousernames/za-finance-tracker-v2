231c2926726219061ed0376505fb9b2594b23847
Therearenousernames <njomokhethelo@gmail.com>
2026-03-21 00:51:44 +0200

    Day 7 complete - Transactions page with add, edit and delete

--- finance-tracker-api/Program.cs
+++ finance-tracker-api/Program.cs
@@ -81,6 +81,11 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
+
 
 var app = builder.Build();
 
