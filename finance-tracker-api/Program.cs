463d545b48bc3948b5fb97b9c1335df5598c61a2
Therearenousernames <njomokhethelo@gmail.com>
2026-03-21 04:10:58 +0200

    Add auto-migration on startup for production deployment

--- finance-tracker-api/Program.cs
+++ finance-tracker-api/Program.cs
@@ -96,10 +96,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+
+// Auto-migrate database on startup 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
